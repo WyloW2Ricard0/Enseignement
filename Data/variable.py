ef21b077ec6765e15ce1c04c9d324b2b2eb49212
WyloW <wrichard@live.fr>
2025-12-04 13:44:53 +0100

    Initial commit
    newsetup project structure with documentation

---
+++ Data/variable.py
@@ -0,0 +1,11 @@
+## Github configuration
+# Nom et email utilisés pour les commits Git
+GIT_USER_NAME = "WyloW"
+GIT_USER_EMAIL = "wrichard@live.fr"
+
+# Commande pour ouvrir VS Code en mode édition de message de commit
+GIT_EDITOR_VSCODE = "code --wait" # Le --wait est crucial : sans lui, VS Code s'ouvrirait mais Git continuerait immédiatement sans attendre que vous ayez terminé d'écrire votre message — le résultat serait un commit vide ou annulé.
+
+## Projet configuration
+# Nom du chemain
+PATH_PROJET = "F:\Enseignement>"
